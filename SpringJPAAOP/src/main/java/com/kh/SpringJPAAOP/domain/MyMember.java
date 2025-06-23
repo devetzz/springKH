748ec7d7e3efe3e27afb044ad6dc3eeae338bd67
parkkangho <etzz@naver.com>
2025-06-23 18:00:00 +0900

    2025.06.23 [Update] Spring JPA AOP

--- SpringJPAAOP/src/main/java/com/kh/SpringJPAAOP/domain/MyMember.java
+++ SpringJPAAOP/src/main/java/com/kh/SpringJPAAOP/domain/MyMember.java
@@ -1,7 +1,6 @@
 package com.kh.SpringJPAAOP.domain;
 
 import java.util.Date;
-import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
